213c5feda27fd7743f6b1117b12cdac5300c6e79
rotshidzw <69056906+rotshidzw@users.noreply.github.com>
2022-05-16 12:46:04 +0200

    adding things

---
+++ index.js
@@ -0,0 +1,68 @@
+
+var a = 33 ;
+var b =  10;
+var c = "test";
+var d = "<br />";
+
+
+document.write("a + b = ");
+
+res = a + b ;
+
+document.write(res);
+document.write(d);
+
+document.write("a - b = ");
+
+res = a - b ;
+
+document.write(res);
+document.write(d);
+
+document.write("a * b = ");
+
+res = a - b ;
+
+document.write(res);
+document.write(d);
+
+document.write("a / b = ");
+
+res = a / b ;
+
+document.write(res);
+document.write(d);
+
+document.write("a % b = ");
+
+res = a % b ;
+
+document.write(res);
+document.write(d);
+
+
+document.write("a + b + c = ");
+
+res = a + b + c ;
+
+document.write(res);
+document.write(d);
+
+document.write("a++ = ");
+
+res = a++ ;
+
+document.write(res);
+document.write(d);
+
+document.write("a-- = ");
+
+res =  a-- ;
+
+document.write(res);
+document.write(d);
+
+
+   
+
+
